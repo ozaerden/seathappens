41a80d3d304895e0b7e4f7e7136577505e7f2d0f
Ozan Erden <ozaerden@hotmail.com>
2026-05-20 15:20:56 +0300

    DB migration order fix.

--- src/main/resources/db/migration/V7__create_reservations_table.sql
+++ src/main/resources/db/migration/V7__create_reservations_table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE reservations (
                               id UUID PRIMARY KEY,
 
+                              user_id UUID NOT NULL,
                               ticket_type_id UUID NOT NULL,
 
                               quantity INTEGER NOT NULL,
@@ -11,7 +12,14 @@ CREATE TABLE reservations (
                               updated_at TIMESTAMP,
                               version BIGINT,
 
+                              CONSTRAINT fk_reservations_user
+                                  FOREIGN KEY (user_id)
+                                      REFERENCES users(id),
+
                               CONSTRAINT fk_reservations_ticket_type
                                   FOREIGN KEY (ticket_type_id)
                                       REFERENCES ticket_types(id)
 );
+
+CREATE INDEX idx_reservations_user_id
+    ON reservations(user_id);
